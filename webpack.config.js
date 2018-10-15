3b8331a37fd4218bdbf7b385af7f2c2c3e63b5c2
LengYingxin <lengyingxin8966@gmail.com>
2018-10-15 17:14:03 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('./config/webpack.config');
+const config = {
+    secure: false,
+    changeOrigin: true,
+    logLevel: "debug"
+};
+module.exports = webpack(env => {
+    return {
+        port: 8099,
+        proxy: {
+            /**
+            * 脚手架服务器地址
+            */
+            '/server': {
+                target: 'http://localhost:8765',
+                // pathRewrite: {
+                //     "^/server": ""
+                // },
+                ...config
+            },
+            /**
+             * swaggerDoc 代理地址
+             */
+            '/swaggerDoc': {
+                target: 'http://localhost:8765/api-docs',
+                pathRewrite: {
+                    "^/swaggerDoc": ""
+                },
+                ...config
+            },
+        },
+        deployWrite: `
+        <!--             写点什么进去？             -->
+        <script>
+            console.log("写点什么？");
+        </script>
+        `,
+        // 字符串写入 需要 使用 JSON.stringify 转换
+        DefinePlugin: {
+            APIADDRESS: JSON.stringify(env.type == "deploy" ? "/api" : "/api"),
+        }
+    }
+})
